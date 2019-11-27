7d72818b036e1f295ff4cdc7caa2caeb0bfd5fcc
Sujan Maharjan <smaharjan540>
2019-11-27 14:23:27 -0600

    test

--- src/main/java/com/dream/project/controller/LedController.java
+++ src/main/java/com/dream/project/controller/LedController.java
@@ -8,35 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class LedController {
 
     private static GpioPinDigitalOutput pin;
-    private static GpioPinDigitalOutput pin1;
 
     @RequestMapping("/")
     public String greeting() {
         return "Hello world!";
     }
 
-    @RequestMapping("/fourOn")
-    public String oneOn() {
-
-        if (pin1 == null) {
-            GpioController gpio = GpioFactory.getInstance();
-            pin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.HIGH);
-        }
-        pin1.toggle();
-
-        return "OK";
-    }
-
-    @RequestMapping("/fourOff")
-    public String oneOff() {
-
-        if (pin1 == null) {
-            GpioController gpio = GpioFactory.getInstance();
-            pin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.LOW);
-        }
-        pin1.toggle();
-        return "OK";
-    }
 
     @RequestMapping("/light")
     public String light() {
